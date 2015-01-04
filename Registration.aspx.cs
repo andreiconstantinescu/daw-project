eb17cadc2d56b13aa0953e0c403bf1c7f074390f
Andrei Constantinescu <andrei@constantinescu.io>
2015-01-04 11:59:24 +0200

    Add management page and DB.

--- Registration.aspx.cs
+++ Registration.aspx.cs
@@ -4,15 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class Registration : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (IsPostBack)
+        {
+            SqlConnection usrData = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+            usrData.Open();
+            string query = "select count(*) from userData where username='" + txtb_username.Text + "'";
+            SqlCommand command = new SqlCommand(query, usrData);
+            int temp = Convert.ToInt32(command.ExecuteScalar().ToString());
+            if (temp == 1)
+            {
+                Response.Write("Username already Exists.");
+            }
+            usrData.Close();
+        }
     }
     protected void btn_register_Click(object sender, EventArgs e)
     {
-        Response.Write("Your registration was successful.");
+        try
+        {
+            SqlConnection usrData = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+            usrData.Open();
+            string insertQuery = "insert into userData (username, email, password, role) values (@username, @email, @password, @role)";
+            SqlCommand command = new SqlCommand(insertQuery, usrData);
+
+            command.Parameters.AddWithValue("@username", txtb_username.Text);
+            command.Parameters.AddWithValue("@email", txtb_email.Text);
+            command.Parameters.AddWithValue("@password", txtb_pass.Text);
+            command.Parameters.AddWithValue("@role", dlst_role.SelectedItem.ToString());
+
+            command.ExecuteNonQuery();
+
+            Response.Write("Registration is successful!");
+            
+            usrData.Close();
+        }
+        catch(Exception ex)
+        {
+            Response.Write("Error: " + ex.Message);
+        }
     }
 }
